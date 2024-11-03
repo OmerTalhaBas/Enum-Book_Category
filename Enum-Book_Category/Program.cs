c91d9149a7e9753b68e5333252fc119c20e5a3d9
OmerTalhaBas <omertalhabas58@gmail.com>
2024-11-03 17:22:59 +0300

    Add project files.

---
+++ Enum-Book_Category/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    enum KitapKategori
+    {
+        BilimKurgu = 0,
+        DunyaKlasikleri = 1,
+        Psikoloji = 2
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen Bir Kategori Giriniz \n0 - BilimKurgu\n1 - DunyaKlasikleri\n2 - Psikoloji \nYa Kategori İsimini Ya Da Kategori Numarasını Girerek İstediğiniz Reyon Hakkında Bilgi Alabilirsiniz.\nLütfen Giriniz : ");
+        string kategoriGiris = Console.ReadLine();
+        Console.Clear();
+        Thread.Sleep(1000);
+        Console.WriteLine("Reyon Hakkında Bilgi Yükleniyor...");
+        Thread.Sleep(1000);
+        Console.Clear();
+        try
+        {
+            KitapKategori kategori = (KitapKategori)Enum.Parse(typeof(KitapKategori), kategoriGiris);
+            switch (kategori)
+            {
+                case KitapKategori.BilimKurgu:
+                    Console.WriteLine("Bilim Kurgu Kategorisindeki Kitaplar A Reyonunda Yer Almaktadır.");
+                    break;
+                case KitapKategori.DunyaKlasikleri:
+                    Console.WriteLine("Dünya Klasikleri Kategorisindeki Kitaplar B Reyonunda Yer Almaktadır.");
+                    break;
+                case KitapKategori.Psikoloji:
+                    Console.WriteLine("Psikoloji Kategorisindeki Kitaplar C Reyonunda Yer Almaktadır.");
+                    break;
+            }
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Geçersiz Kategori Girdişi Yaptınız. Lütfen Doğru Bir Kategori Giriniz.");
+        }
+    }
+}
